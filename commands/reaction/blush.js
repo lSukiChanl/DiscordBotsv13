e63742e66cbcf8dd7c57165ca1e5d21ef9fbb32a
Suki-Chan <crtmg@outlook.com>
2022-06-23 14:30:24 -0500

    nueva reaccion

---
+++ commands/reaction/blush.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+    name: "blush",
+    aliases: ["sonrojado", "apenado", "pena", "verguenza"],
+    description:"Reaction Anime Angry/Molesto",
+    
+    async execute (client, message, args, discord){
+        try {
+            const Creditos = process.env.AUTHOR;
+            let NumMagic = Math.floor(Math.random()*105);
+            NumMagic = NumMagic + 1;            
+            let embed = new discord.MessageEmbed()
+                embed.setColor('#D57DC1')
+                embed.setDescription(" "+msg.author.username + " 𝐄𝐬𝐭𝐚 𝐒𝐨𝐧𝐫𝐨𝐣𝐚𝐝@ (⁄ ⁄•⁄ω⁄•⁄ ⁄) " )
+                embed.setImage("https://www.ichimonogatari.com/IMG/Menhera%20Bots/Reaction%20Commands/Blush/Blush"+NumMagic+".gif");
+                embed.setFooter({text: " 𝐂𝐫𝐞𝐝𝐢𝐭𝐨𝐬 : " + Creditos})
+                embed.setTimestamp()
+            return message.channel.send( {embeds: [embed] });
+        } catch (error) {
+            return console.log("Error : " + error);
+        }
+    }
+};
